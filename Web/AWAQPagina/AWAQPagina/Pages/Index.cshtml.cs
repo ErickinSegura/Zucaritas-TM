b3b72eb7bc1c493c17c02c565fe7e61631cae73c
herveynavarro04 <herveynavarro2004@gmail.com>
2024-03-14 20:12:54 -0600

    Creacion de Backend del index (Log in) y primera muestra del frontend del mismo

--- Web/AWAQPagina/AWAQPagina/Pages/Index.cshtml.cs
+++ Web/AWAQPagina/AWAQPagina/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace AWAQPagina.Pages;
 
@@ -12,9 +13,44 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+   /* public void OnPost()
+    {
+        string connectionString = "Server=127.0.0.1;Port=3306;Database=AWAQ;Uid=root;password=Vela0376;";
+        string usuario = Request.Form["usuario"];
+        string contraseña = Request.Form["contraseña"];
+       
+        string sql = "SELECT COUNT(*) FROM USUARIO WHERE Usuario == @usuario AND Contraseña == @contraseña";
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+
+                    using(MySqlCommand command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@username", usuario);
+                    command.Parameters.AddWithValue("@password", contraseña);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        Response.Redirect("");
+                    }
+                }
+
+          
+            }
+        }
+
+
+
+    }*/
+
     public void OnGet()
     {
 
     }
-}
 
+}
