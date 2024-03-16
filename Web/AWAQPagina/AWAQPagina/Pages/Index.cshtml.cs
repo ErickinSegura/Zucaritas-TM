6922d543e65f5d36938aa6197529793cf15c1773
herveynavarro04 <herveynavarro2004@gmail.com>
2024-03-15 18:43:28 -0600

    Agregue el backend de studentView y cambie el color del boton en perfil a color verde

--- Web/AWAQPagina/AWAQPagina/Pages/Index.cshtml.cs
+++ Web/AWAQPagina/AWAQPagina/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
 using System;
 
 namespace AWAQPagina.Pages
@@ -10,7 +11,7 @@ namespace AWAQPagina.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly string _connectionString = "Server=127.0.0.1;Port=3306;Database=AWAQ;Uid=root;password=lala14;";
+        private readonly string _connectionString = "Server=127.0.0.1;Port=3306;Database=AWAQ;Uid=root;password=Vela0376;";
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         public IndexModel(ILogger<IndexModel> logger, IHttpContextAccessor httpContextAccessor)
@@ -20,6 +21,9 @@ namespace AWAQPagina.Pages
         }
 
         public string UserId { get; private set; }
+        public string nombre{ get; private set; }
+        public string apellido { get; private set; }
+        public string nombreUsuario { get; private set; }
 
         public IActionResult OnPost()
         {
@@ -28,6 +32,9 @@ namespace AWAQPagina.Pages
 
             string sql = "SELECT COUNT(*) FROM USUARIO WHERE Usuario = @usuario AND Contraseña = @contraseña";
             string sql2 = "SELECT ID_USER FROM USUARIO WHERE Usuario = @usuario AND Contraseña = @contraseña";
+            string sql3 = "SELECT Nombre FROM USUARIO WHERE Usuario = @usuario AND Contraseña = @contraseña";
+            string sql4 = "SELECT APELLIDO_PATERNO FROM USUARIO WHERE Usuario = @usuario AND Contraseña = @contraseña";
+            string sql5 = "SELECT Usuario FROM USUARIO WHERE Usuario = @usuario AND Contraseña = @contraseña";
 
             using (MySqlConnection connection = new MySqlConnection(_connectionString))
             {
@@ -40,6 +47,7 @@ namespace AWAQPagina.Pages
                     command.Parameters.AddWithValue("@contraseña", contraseña);
 
                     int count = Convert.ToInt32(command.ExecuteScalar());
+
 
                     if (count > 0)
                     {
@@ -55,9 +63,55 @@ namespace AWAQPagina.Pages
                                 UserId = result.ToString(); // Store the user ID in the cookie
 
                                 // Set the UserId cookie
-                                _httpContextAccessor.HttpContext.Response.Cookies.Append("UserId", UserId);
+                                _httpContextAccessor.HttpContext.Response.Cookies.Append("ID_USER", UserId);
                             }
                         }
+
+
+                        using (MySqlCommand command3 = new MySqlCommand(sql3, connection))
+                        {
+                            command3.Parameters.AddWithValue("@usuario", usuario);
+                            command3.Parameters.AddWithValue("@contraseña", contraseña);
+
+                            object result = command3.ExecuteScalar();
+
+                            if (result != null)
+                            {
+                                nombre = result.ToString();
+                                _httpContextAccessor.HttpContext.Response.Cookies.Append("Nombre", nombre);
+
+                            }
+                        }
+
+                        using (MySqlCommand command4 = new MySqlCommand(sql4, connection))
+                        {
+                            command4.Parameters.AddWithValue("@usuario", usuario);
+                            command4.Parameters.AddWithValue("@contraseña", contraseña);
+
+                            object result = command4.ExecuteScalar();
+
+                            if (result != null)
+                            {
+                                apellido = result.ToString();
+                                _httpContextAccessor.HttpContext.Response.Cookies.Append("APELLIDO_PATERNO", apellido);
+                            }
+                        }
+
+
+                        using (MySqlCommand command5 = new MySqlCommand(sql5, connection))
+                        {
+                            command5.Parameters.AddWithValue("@usuario", usuario);
+                            command5.Parameters.AddWithValue("@contraseña", contraseña);
+
+                            object result = command5.ExecuteScalar();
+
+                            if (result != null)
+                            {
+                                nombreUsuario = result.ToString();
+                                _httpContextAccessor.HttpContext.Response.Cookies.Append("Usuario", nombreUsuario);
+                            }
+                        }
+
 
                         // Redirect to studentView page
                         return RedirectToPage("/studentView");
