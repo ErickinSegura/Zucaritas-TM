2be2ec2044633027a4afd2a5269d39b3dc5a0c88
Saul0Delgado <A01285188@tec.mx>
2024-04-11 01:12:59 -0600

    Le avancé xD
    Pajaros y mamiferos

--- Videojuego/GamiAwaq/Assets/Scripts/Tree/TreeController.cs
+++ Videojuego/GamiAwaq/Assets/Scripts/Tree/TreeController.cs
@@ -2,9 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TreeController : MonoBehaviour
 {
+    public enum Direction { N, E, S, O };
+
+    public Direction currentDirection = Direction.N;
+    public Text directionText;
+
+    void Start()
+    {
+        UpdateDirectionText();
+    }
+
+    void UpdateDirectionText()
+    {
+        directionText.text = currentDirection.ToString();
+    }
+
+    public void ChangeDirection(int directionChange)
+    {
+        int directionCount = System.Enum.GetValues(typeof(Direction)).Length;
+        int newDirectionIndex = ((int)currentDirection + directionChange + directionCount) % directionCount;
+        currentDirection = (Direction)newDirectionIndex;
+        UpdateDirectionText();
+    }
+
     public void MamiferosLoad()
     {
         SceneManager.LoadScene("Mamiferos");
