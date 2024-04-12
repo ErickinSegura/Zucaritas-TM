2e472fca9a8cf2964f4991eae15c6418576e4ac0
ErickinSegura <A01613821@tec.mx>
2024-04-12 15:50:45 -0600

    API añadida

---
+++ Web/AWAQApi/RegistroEspecie.cs
@@ -0,0 +1,12 @@
+﻿namespace AWAQApi
+{
+    public class RegistroEspecie
+    {
+        public int ID_Registro { get; set; }
+        public int ID_Especie { get; set; }
+        public int ID_Usuario { get; set; }
+        public int ID_Muestreo { get; set; }
+        public DateTime Fecha { get; set; }
+
+    }
+}
