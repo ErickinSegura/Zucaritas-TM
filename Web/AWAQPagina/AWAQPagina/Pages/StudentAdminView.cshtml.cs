2d87fcdcdce25485b1353cb70ca807abd094ae93
herveynavarro04 <herveynavarro2004@gmail.com>
2024-04-29 14:40:35 -0600

    cambios y limpieza de codigo

--- Web/AWAQPagina/AWAQPagina/Pages/StudentAdminView.cshtml.cs
+++ Web/AWAQPagina/AWAQPagina/Pages/StudentAdminView.cshtml.cs
@@ -11,19 +11,16 @@ namespace AWAQPagina.Pages
 {
 	public class StudentAdminViewModel : PageModel
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public Usuario usuario { get; set; }
-        public string dashboardLink { get; set; }
+        public string? dashboardLink { get; set; }
 
-        public StudentAdminViewModel(IHttpContextAccessor httpContextAccessor)
+        public StudentAdminViewModel()
         {
-            _httpContextAccessor = httpContextAccessor;
             usuario = new Usuario();
-
         }
 
-        public IActionResult OnGet(string id)
+        public IActionResult OnGet(string idParam)
         {
             if (HttpContext.Session.GetString("Role") != "Admin")
             {
@@ -40,7 +37,7 @@ namespace AWAQPagina.Pages
 
             else
             {
-                string userID = id; //id que se esta buscando  
+                string userID = idParam;
                 string connectionString = System.IO.File.ReadAllText(".connectionstring.txt");
                 MySqlConnection conexion = new MySqlConnection(connectionString);
 
@@ -60,13 +57,14 @@ namespace AWAQPagina.Pages
                         usuario.firstLastname = reader["APELLIDO_PATERNO"].ToString();
                         usuario.userName = reader["Usuario"].ToString();
                         usuario.profilePicture = reader["Imagen_USUARIO"].ToString().Substring(1);
+                        usuario.bio = reader["BIO"].ToString();
+
                     }
+
+                    conexion.Close();
+    
                 }
 
-                usuario.bio = "Biografia placeholder";
-                // Agregar query
-
-                
                 dashboardLink = String.Format("https://lookerstudio.google.com/embed/reporting/ed3ae5e0-9da6-401f-8fbb-f4fd23a6d451/page/E3ZwD?params=%7B%22ds21.iduser%22%3A{0}%2C%22ds5.iduserbar%22%3A{0}%7D", userID);
 
                 return Page();
