56d4178ccb706fcd9bce4d8de770ad98c13e210c
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2024-04-29 11:24:12 -0600

    reverted lost changes

---
+++ Web/AWAQPagina/AWAQPagina/Pages/Especie.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWAQPagina.Pages
+{
+    public class EspecieModel : PageModel
+    {
+        public string? IdEspecie { set; get; }
+        public Especie? especie { set; get; }
+
+        public void OnGet(string idEspecieParam)
+        {
+            IdEspecie = idEspecieParam;
+
+            especie = new Especie();
+
+            especie.nombre_Especie = "Caimán Aguja";
+            especie.img_web = "https://inaturalist-open-data.s3.amazonaws.com/photos/3715/large.jpg";
+            especie.descripcion = "Es una especie de cocodrílido que vive en Florida, algunas islas del Mar Caribe y varias zonas costeras del golfo de México y el océano Pacífico. Se le suele confundir con el Alligator, pero éste último es un animal poco emparentado con el anterior, más próximo a los caimanes como el yacaré overo. Las diferencias físicas entre ambos son importantes, lo que permite identificarlos";
+            especie.nombre_cientifico = "Crocodylus acutus";
+            especie.ID_Especie = "4";
+        }
+    }
+}
---
+++ Web/AWAQPagina/AWAQPagina/Pages/StudentAdminView.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace AWAQPagina.Pages
+{
+	public class StudentAdminViewModel : PageModel
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public Usuario usuario { get; set; }
+        public string dashboardLink { get; set; }
+
+        public StudentAdminViewModel(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            usuario = new Usuario();
+
+        }
+
+        public IActionResult OnGet(string id)
+        {
+            //if (HttpContext.Session.GetString("Role") != "Student")
+            //{
+            //    if (HttpContext.Session.GetString("Role") != "Admin")
+            //    {
+            //        return RedirectToPage("Index");
+            //    }
+
+            //    else
+            //    {
+            //        return RedirectToPage("/adminView");
+            //    }
+            //}
+
+            //else
+            //{              Rehacer logica para solo admin
+                string userID = id;  //userID es id del que se esta visualizando
+                string connectionString = System.IO.File.ReadAllText("../.connectionstring.txt");
+                MySqlConnection conexion = new MySqlConnection(connectionString);
+
+                conexion.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "get_user_info";
+                cmd.Connection = conexion;
+
+                cmd.Parameters.AddWithValue("@userID", userID);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        usuario.name = reader["Nombre"].ToString();
+                        usuario.firstLastname = reader["APELLIDO_PATERNO"].ToString();
+                        usuario.userName = reader["Usuario"].ToString();
+                        usuario.profilePicture = reader["Imagen_USUARIO"].ToString().Substring(1);
+                    }
+                }
+
+                usuario.bio = "Biografia placeholder";
+                // Agregar query
+
+                
+                dashboardLink = String.Format("https://lookerstudio.google.com/embed/reporting/ed3ae5e0-9da6-401f-8fbb-f4fd23a6d451/page/E3ZwD?params=%7B%22ds21.iduser%22%3A{0}%2C%22ds5.iduserbar%22%3A{0}%7D", userID);
+
+                return Page();
+            //}
+        }
+    }
+}
