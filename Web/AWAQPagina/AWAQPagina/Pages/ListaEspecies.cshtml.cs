b325b6438fbee8dc9e5992971a1880f85e301ff9
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2024-04-29 13:39:23 -0600

    listaEspecies avance

--- Web/AWAQPagina/AWAQPagina/Pages/ListaEspecies.cshtml.cs
+++ Web/AWAQPagina/AWAQPagina/Pages/ListaEspecies.cshtml.cs
@@ -9,8 +9,23 @@ namespace AWAQPagina.Pages
 {
 	public class ListaEspeciesModel : PageModel
     {
+        public List<Especie>? especies { set; get; }
+
+
         public void OnGet()
         {
+            especies = new List<Especie>();
+            for (int i=0; i<3; i++)
+            {
+                Especie especie = new Especie();
+                especie.nombre_Especie = "nombre" + i.ToString();
+                especie.nombre_cientifico = "cientifico" + i.ToString();
+                especie.descripcion = "descripcion" + i.ToString();
+                especie.img_web = "/images/1.jpg";
+
+                especies.Add(especie);
+
+            }
         }
     }
 }
